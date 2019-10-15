8161ec98281310953ff57a032de49b233177898c
mindcontago <55882050+mindcontago@users.noreply.github.com>
2019-10-15 15:13:19 +0300

    Add files via upload

---
+++ springproject/src/main/java/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import org.springframework.stereotype.Component;
+
+import javax.xml.ws.Service;
+
+@Controller
+@Component
+public class Controller {
+    final Service service;
+}
---
+++ springproject/src/main/java/service/Main.java
@@ -0,0 +1,8 @@
+package service;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        StringApplication.run(Main.class, args);
+    }
+}
