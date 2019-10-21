00b41bdae7434b24aab31a1b75b967ceecfd6c61
mindcontago <55882050+mindcontago@users.noreply.github.com>
2019-10-21 18:30:15 +0300

    Add files via upload

--- springproject/src/main/java/service/Main.java
+++ springproject/src/main/java/service/Main.java
@@ -2,6 +2,9 @@ package service;
 
 
 import config.ConfigApp;
+import controller.TaskController;
+import controller.UserController;
+import model.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -15,13 +18,25 @@ public class Main {
         context.register(ConfigApp.class);
         context.refresh();
 
+        UserController userController = context.getBean(UserController.class);
+        TaskController taskController = context.getBean(TaskController.class);
+
+        User testUser = User.builder().userId((long) 1)
+                .name("James")
+                .surname("Bond")
+                .email("bond@aol.com")
+                .passowrd("333").build();
+
+        userController.signUp(testUser);
+        userController.signIn(testUser);
+
 //        House bean = context.getBean(House.class);
 //        bean.method();
     }
 
 
     //For spring convert top JSON
-    
+
     protected void configMessageConvert(List<HttpMessageConverter<?>> converters){
         converters.add(new MappingJackson2HttpMessageConverter());
     }
